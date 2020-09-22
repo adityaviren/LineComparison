75089f2deb487cd661674be5e098d04863c26220
adityaviren <adityaviren@gmail.com>
2020-09-22 12:35:24 +0530

    UC3 using CompareTo in child1

--- src/LineComp/LineComparisonCartisan.java
+++ src/LineComp/LineComparisonCartisan.java
@@ -8,6 +8,7 @@ public class LineComparisonCartisan {
 		System.out.println("Welcome to Line Comparison Computation Program");
 		interact1();
 		interact2();
+		interact3();
 	}
 	private static void interact1() {
 		Scanner sc=new Scanner(System.in);
@@ -20,6 +21,29 @@ public class LineComparisonCartisan {
 		System.out.println("Enter the y-coordinate of 2nd point");
 		int y2=sc.nextInt();
 		System.out.println("Lenth of line is "+lengthOfLine(x1, x2, y1, y2));
+	}
+	private static void interact3() {
+		Scanner sc=new Scanner(System.in);
+		String length_line[]=new String[2];
+		for(int i=0;i<2;i++) {
+			System.out.println("For Line "+(i+1));
+			System.out.println("Enter the x-coordinate of 1st point");
+			int x1=sc.nextInt();
+			System.out.println("Enter the y-coordinate of 1st point");
+			int y1=sc.nextInt();
+			System.out.println("Enter the x-coordinate of 2nd point");
+			int x2=sc.nextInt();
+			System.out.println("Enter the y-coordinate of 2nd point");
+			int y2=sc.nextInt();
+			length_line[i]=Double.toString(lengthOfLine(x1, x2, y1, y2));
+		}
+		int check_compareTO = length_line[0].compareTo(length_line[1]);
+		if(check_compareTO==0)
+			System.out.println("The lines are equal in length.");
+		else if(check_compareTO<0)
+			System.out.println("Line 2 is longer than Line 1");
+		else 
+			System.out.println("Line 1 is longer than Line 1");
 	}
 	private static void interact2() {
 		Scanner sc=new Scanner(System.in);
