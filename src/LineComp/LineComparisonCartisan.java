8633fda0f2956426e2f10ffdd3f91204d28af466
adityaviren <adityaviren@gmail.com>
2020-09-22 11:54:06 +0530

    Master branch

--- src/LineComp/LineComparisonCartisan.java
+++ src/LineComp/LineComparisonCartisan.java
@@ -1,8 +1,11 @@
 package LineComp;
 
+import java.util.Scanner;
+
 public class LineComparisonCartisan {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Welcome to Line Comparison Computation Program");
 		
 	}
 }
