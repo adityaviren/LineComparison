38c70c4a58fe5d6dd6d29b3798cbcf9c65c39c91
unknown <adityaviren@gmail.com>
2020-09-29 20:26:21 -0700

    added compareTo use and user input

--- src/LineComp/LineComparisonCartisan.java
+++ src/LineComp/LineComparisonCartisan.java
@@ -4,7 +4,26 @@ import java.util.Scanner;
 
 public class LineComparisonCartisan {
 	public static void main(String args[]) {
-		equalsToLineLength();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to Line Comparison Computation Program\nEnter 1 to calculate length of line"
+				+ "\nEnter 2 to check if the length of 2 lines are equal"
+				+ "\nEnter 3 to compare the length of 2 lines");
+		int user_option = sc.nextInt();
+		switch (user_option) {
+		case 1:
+			System.out.println("Length of line is " + input1());
+			break;
+		case 2:
+			equalsToLineLength();
+			break;
+		case 3:
+			compareToLineLength();
+			break;
+		default:
+			break;
+		}
+		sc.close();
+
 	}
 
 	private static double input1() {
@@ -21,6 +40,22 @@ public class LineComparisonCartisan {
 		length_of_line = lengthOfLine(x1, x2, y1, y2);
 		return length_of_line;
 	}
+
+	private static void compareToLineLength() {
+		Double length_line[] = new Double[2];
+		for (int i = 0; i < 2; i++) {
+			System.out.println("For Line " + (i + 1));
+			length_line[i] =(input1());
+		}
+		int check_compareTO = length_line[0].compareTo(length_line[1]);
+		if (check_compareTO == 0)
+			System.out.println("The lines are equal in length.");
+		else if (check_compareTO < 0)
+			System.out.println("Line 2 is longer than Line 1");
+		else
+			System.out.println("Line 1 is longer than Line 1");
+	}
+
 	private static void equalsToLineLength() {
 		Double length_line[] = new Double[2];
 		for (int i = 0; i < 2; i++) {
