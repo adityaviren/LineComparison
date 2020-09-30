9b5709f012edae2a18764d4dec19a1ea1ef99955
unknown <adityaviren@gmail.com>
2020-09-29 20:21:46 -0700

    added equal to method

--- src/LineComp/LineComparisonCartisan.java
+++ src/LineComp/LineComparisonCartisan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LineComparisonCartisan {
 	public static void main(String args[]) {
-		System.out.println(input1());
+		equalsToLineLength();
 	}
 
 	private static double input1() {
@@ -20,6 +20,17 @@ public class LineComparisonCartisan {
 		int y2 = sc.nextInt();
 		length_of_line = lengthOfLine(x1, x2, y1, y2);
 		return length_of_line;
+	}
+	private static void equalsToLineLength() {
+		Double length_line[] = new Double[2];
+		for (int i = 0; i < 2; i++) {
+			System.out.println("For Line " + (i + 1));
+			length_line[i] = (input1());
+		}
+		if (length_line[0].equals(length_line[1]))
+			System.out.println("The lines are equal in length.");
+		else
+			System.out.println("The lines are unequal in length.");
 	}
 
 	private static double lengthOfLine(int x1, int x2, int y1, int y2) {
